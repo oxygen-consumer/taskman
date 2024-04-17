01a7d5e59dd8d2d93bd43926e3dfd9b0b564268a
Stefan Zaharie <stefan@zaha.tech>
2024-04-17 14:46:50 +0300

    DRAFT: add db and some placeholder items

---
+++ TaskmanAPI/Models/User.cs
@@ -0,0 +1,13 @@
+namespace TaskmanAPI.Model;
+
+// DRAFT
+public class User
+{
+    public int id;
+
+    public string username { get; set; }
+
+    public string password { get; set; }
+
+    public string email { get; set; }
+}
