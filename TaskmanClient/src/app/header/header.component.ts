3c6c02dec3a982a37e3199620b3f47e0ed1a9ab0
Vizinteanu Teodora <115303402+TeoVizi@users.noreply.github.com>
2024-04-26 12:25:01 +0300

    header

--- TaskmanClient/src/app/header/header.component.ts
+++ TaskmanClient/src/app/header/header.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {NgOptimizedImage} from "@angular/common";
 
 @Component({
   selector: 'app-header',
     standalone: true,
-    imports: [RouterOutlet],
+  imports: [RouterOutlet, NgOptimizedImage],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
