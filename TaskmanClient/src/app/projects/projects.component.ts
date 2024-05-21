799a861de7a96c046c8ba396dcc843740e4e7eb7
DanAlexandruS <116816063+DanAlexandruS@users.noreply.github.com>
2024-05-22 00:17:31 +0300

    porjects tab

--- TaskmanClient/src/app/projects/projects.component.ts
+++ TaskmanClient/src/app/projects/projects.component.ts
@@ -1,17 +1,34 @@
-﻿import {Component,OnInit} from '@angular/core';
+﻿import {Component, OnInit, QueryList, ViewChild, ViewChildren} from '@angular/core';
 import {LoginServiceService} from "../service/login-service.service";
 import {ProjectService} from "../service/project-service.service";
+import {Table, TableModule} from "primeng/table";
+import {InputTextModule} from "primeng/inputtext";
+import {FormsModule} from "@angular/forms";
+import {RippleModule} from "primeng/ripple";
+import {ButtonModule} from "primeng/button";
+import {NgIf} from "@angular/common";
 
 
 @Component({
   selector: 'app-projects',
   standalone: true,
   templateUrl: './projects.component.html',
+  imports: [
+    TableModule,
+    InputTextModule,
+    FormsModule,
+    RippleModule,
+    ButtonModule,
+    NgIf
+  ],
   styleUrl: './projects.component.scss'
 })
 export class ProjectsComponent {
   data:any;
   token:any;
+  @ViewChild('dt') table: Table;
+  useTable:any;
+  clonedRows:{[s:string]:Projects} = {};
   accesToken = "acces_token";
   refreshToken = "refresh_token";
   constructor(private service:ProjectService){
@@ -26,4 +43,25 @@ export class ProjectsComponent {
         console.log(result);
       })
     }
+
+
+  onRowEditInit(row: any,index:any) {
+    this.clonedRows[index] = {...this.data[index]};
+
+  }
+
+  onRowEditSave(row: any,index:any) {
+    //momentan gol
+  }
+
+  onRowEditCancel(row: any, rowIndex: any) {
+    this.data[rowIndex] = this.clonedRows[rowIndex];
+    delete this.clonedRows[rowIndex];
+  }
+}
+
+export interface Projects {
+  name?: string;
+  description?: string;
+
 }
