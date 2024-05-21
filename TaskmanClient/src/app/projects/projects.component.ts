5a452473b9181d8e8e2620cf828a41fe01da9c1e
DanAlexandruS <116816063+DanAlexandruS@users.noreply.github.com>
2024-05-21 20:22:46 +0300

    register , project si login

---
+++ TaskmanClient/src/app/projects/projects.component.ts
@@ -0,0 +1,18 @@
+﻿import {Component,OnInit} from '@angular/core';
+import {LoginServiceService} from "../service/login-service.service";
+
+
+@Component({
+  selector: 'app-projects',
+  standalone: true,
+  templateUrl: './projects.component.html',
+  styleUrl: './projects.component.scss'
+})
+export class ProjectsComponent {
+   constructor(private service:LoginServiceService){
+
+   }
+    ngOnInit(){
+      console.log("buna");
+    }
+}
