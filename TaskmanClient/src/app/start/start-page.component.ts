dc0c5564d75958fb43c97274c5ef52908383f3d3
Vizinteanu Teodora <115303402+TeoVizi@users.noreply.github.com>
2024-05-21 20:20:43 +0300

    pagina initiala

---
+++ TaskmanClient/src/app/start/start-page.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import {FormBuilder, FormGroup, NgForm, ReactiveFormsModule, Validators} from "@angular/forms";
+import {LoginServiceService} from "../service/login-service.service";
+import {HttpClient, HttpClientModule} from "@angular/common/http";
+import {HeaderComponent} from "../header/header.component";
+
+@Component({
+  selector: 'startpage-component',
+  standalone: true,
+  imports: [RouterOutlet, ReactiveFormsModule, HttpClientModule, HeaderComponent],
+  templateUrl: './start-page.component.html',
+  styleUrl: './start-page.component.scss'
+})
+export class StartPageComponent {
+  registerForm: FormGroup;
+  title: any;
+  password: string | undefined;
+  email: string | undefined;
+
+  constructor(private fb: FormBuilder, private loginService: LoginServiceService) {
+    this.registerForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+    });
+  }
+
+}
