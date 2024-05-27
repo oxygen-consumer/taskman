93a7e5fc46232ac122c81782dc67c2bd9a9f3275
DanAlexandruS <116816063+DanAlexandruS@users.noreply.github.com>
2024-05-27 16:57:16 +0300

    project 2 page

--- TaskmanClient/src/app/service/project-service.service.ts
+++ TaskmanClient/src/app/service/project-service.service.ts
@@ -1,5 +1,5 @@
 ﻿import { Injectable } from '@angular/core';
-import {Observable} from "rxjs";
+import {catchError, Observable, switchMap} from "rxjs";
 import {HttpClient, HttpClientModule, HttpHeaders} from "@angular/common/http";
 import {LoginServiceService} from "./login-service.service";
 
@@ -17,19 +17,40 @@ export class ProjectService {
       return this.http.get<any>(this.baseLink + "/api/Projects", {headers});
   }
 
-  modifyProject(id:any,data:any){
-    let token = this.service.getToken();
-    const headers = new HttpHeaders({
-      'Authorization': `Bearer ${token}`
-    });
-    return this.http.put<any>(this.baseLink+"/api/Projects/" + id ,data,{headers} )
+  // @ts-ignore
+  modifyProject(id: any, data: any): Observable<any> {
+    return this.service.getToken().pipe(
+      switchMap(token => {
+        const headers = new HttpHeaders({
+          'Authorization': `Bearer ${token}`
+        });
+        return this.http.put<any>(this.baseLink + "/api/Projects/" + id, data, { headers }).pipe(
+          catchError(error => {
+            console.error('Error modifying project:', error);
+            // You can handle the error here or rethrow it if necessary
+            throw error;
+          })
+        );
+      })
+    );
   }
-  addProject(data:any){
-    let token = this.service.getToken();
-    const headers = new HttpHeaders({
-      'Authorization': `Bearer ${token}`
-    });
-    return this.http.post<any>(this.baseLink+"/api/Projects/" ,data,{headers} )
+  // @ts-ignore
+
+  addProject(data: any): Observable<any> {
+    return this.service.getToken().pipe(
+      switchMap(token => {
+        const headers = new HttpHeaders({
+          'Authorization': `Bearer ${token}`
+        });
+        return this.http.post<any>(this.baseLink + "/api/Projects/", data, { headers }).pipe(
+          catchError(error => {
+            console.error('Error adding project:', error);
+            // You can handle the error here or rethrow it if necessary
+            throw error;
+          })
+        );
+      })
+    );
   }
 
 
