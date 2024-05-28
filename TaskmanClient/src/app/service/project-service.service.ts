7f4d886d31a87d9d532da04bdaa66658e886d5b0
Vizinteanu Teodora <115303402+TeoVizi@users.noreply.github.com>
2024-05-28 20:16:25 +0300

    butoane management user pe proiect functionales iuhuu

--- TaskmanClient/src/app/service/project-service.service.ts
+++ TaskmanClient/src/app/service/project-service.service.ts
@@ -53,6 +53,91 @@ export class ProjectService {
     );
   }
 
+// @ts-ignore
 
+  addUser(id: any, username: any): Observable<any> {
+    return this.service.getToken().pipe(
+      switchMap(token => {
+        const headers = new HttpHeaders({
+          'Authorization': `Bearer ${token}`
+        });
+        return this.http.post<any>(this.baseLink + "/api/Projects/" + id + "/add_user/" + username , {}, { headers }).pipe(
+          catchError(error => {
+            console.error('Error modifying project:', error);
+            // You can handle the error here or rethrow it if necessary
+            throw error;
+          })
+        );
+      })
+    );
+  }
+
+  removeUser(id: any, username: any): Observable<any> {
+    return this.service.getToken().pipe(
+      switchMap(token => {
+        const headers = new HttpHeaders({
+          'Authorization': `Bearer ${token}`
+        });
+        return this.http.delete<any>(this.baseLink + "/api/Projects/" + id + "/remove_user/" + username , { headers }).pipe(
+          catchError(error => {
+            console.error('Error modifying project:', error);
+            // You can handle the error here or rethrow it if necessary
+            throw error;
+          })
+        );
+      })
+    );
+  }
+
+ promoteUser(id: any, username: any): Observable<any> {
+    return this.service.getToken().pipe(
+      switchMap(token => {
+        const headers = new HttpHeaders({
+          'Authorization': `Bearer ${token}`
+        });
+        return this.http.post<any>(this.baseLink + "/api/Projects/" + id + "/promote_user/" + username , {}, { headers }).pipe(
+          catchError(error => {
+            console.error('Error modifying project:', error);
+            // You can handle the error here or rethrow it if necessary
+            throw error;
+          })
+        );
+      })
+    );
+  }
+
+  transferOwnership(id: any, username: any): Observable<any> {
+    return this.service.getToken().pipe(
+      switchMap(token => {
+        const headers = new HttpHeaders({
+          'Authorization': `Bearer ${token}`
+        });
+        return this.http.post<any>(this.baseLink + "/api/Projects/" + id + "/transfer_ownership/" + username , {},{ headers }).pipe(
+          catchError(error => {
+            console.error('Error modifying project:', error);
+            // You can handle the error here or rethrow it if necessary
+            throw error;
+          })
+        );
+      })
+    );
+  }
+
+  demoteUser(id: any, username: any): Observable<any> {
+    return this.service.getToken().pipe(
+      switchMap(token => {
+        const headers = new HttpHeaders({
+          'Authorization': `Bearer ${token}`
+        });
+        return this.http.post<any>(this.baseLink + "/api/Projects/" + id + "/demote_user/" + username ,{}, { headers }).pipe(
+          catchError(error => {
+            console.error('Error modifying project:', error);
+            // You can handle the error here or rethrow it if necessary
+            throw error;
+          })
+        );
+      })
+    );
+  }
 
 }
