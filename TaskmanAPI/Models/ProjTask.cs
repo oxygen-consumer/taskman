fefd6de6e1fad8849289a94e53616c6ab5aa8732
Stefan Zaharie <stefan@zaha.tech>
2024-06-16 14:38:40 +0300

    optimize models before dto implementation

--- TaskmanAPI/Models/ProjTask.cs
+++ TaskmanAPI/Models/ProjTask.cs
@@ -1,28 +1,25 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Text.Json.Serialization;
-using TaskmanAPI.Models;
 using TaskStatus = TaskmanAPI.Enums.TaskStatus;
 
-namespace TaskmanAPI.Model;
+namespace TaskmanAPI.Models;
 
 public class ProjTask
 {
     [Key] public int Id { get; set; }
 
-    public int ProjectId { get; set; }
+    public required int ProjectId { get; set; }
 
     public int ParentId { get; set; }
 
-    public string Title { get; set; }
+    [MaxLength(50)] public required string Title { get; set; }
 
-    //public string[] Labels { get; set; } 
+    [MaxLength(200)] public required string Description { get; set; }
 
-    public string Description { get; set; }
+    public required DateTime Deadline { get; set; }
 
-    public DateTime Deadline { get; set; }
-
-    public TaskStatus Status { get; set; }
+    public required TaskStatus Status { get; set; }
 
     // make it easier for frontend
     [NotMapped] public string StatusString => Status.ToString();
@@ -30,7 +27,5 @@ public class ProjTask
     //TO DO - comments
     [JsonIgnore] public virtual ICollection<UserTasks> UserTasks { get; set; } = new List<UserTasks>();
 
-    //subtasks
-    //public virtual ICollection<ProjTask> ProjTasks { get; set; }
     [JsonIgnore] public virtual Project? Project { get; set; }
 }
--- TaskmanAPI/Models/User.cs
+++ TaskmanAPI/Models/User.cs
@@ -6,18 +6,7 @@ namespace TaskmanAPI.Model;
 
 public class User : IdentityUser
 {
-    // [Key]
-    // public String Id { get; set; }
-    // [Required(ErrorMessage = "Numele de utilizator este obligatoriu")]
-    // public string UserName { get; set; }
-    // [Required(ErrorMessage = "Parola este obligatorie")]
-    // public string Password { get; set; }
-    // [Required(ErrorMessage = "Adresa de email este obligatorie")]
-    // public string Email { get; set; }
-
-
-    //to add - notification, comment, reaction (?)
-    public virtual ICollection<UserTasks> UserTasks { get; set; } = new List<UserTasks>();
+    [JsonIgnore] public virtual ICollection<UserTasks> UserTasks { get; set; } = new List<UserTasks>();
 
     [JsonIgnore] public virtual ICollection<RolePerProject>? RolePerProjects { get; set; }
 }
